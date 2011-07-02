fca689dddca4b2655225694a9b93b29529cdb6e2
Xavier Noria <fxn@hashref.com>
2011-07-02 14:51:49 +0200

    Exercise 1.2.2

---
+++ chapter01/exercise-1.2.2.pl
@@ -0,0 +1,9 @@
+% ---\
+%
+% In the previous example, we presented "In Georgia" as a property of Atlanta. Write a
+% Prolog fact that represents "in" as a relation between Atlanta and Georgia.
+%
+% ---/
+
+% We need a 2-place predicate for that. For example,
+located_in(georgia, atlanta).
