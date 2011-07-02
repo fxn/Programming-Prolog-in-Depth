d3fe2d0fd46fe2b2ccddd7b21098b20a09b56f74
Xavier Noria <fxn@hashref.com>
2011-07-02 14:54:59 +0200

    Exercise 1.2.3

---
+++ chapter01/exercise-1.2.3.pl
@@ -0,0 +1,10 @@
+% ---\
+%
+% How would you represent, in Prolog, the fact "Atlanta is at latitude 34 north and
+% longitude 84 west"? (Hint: More than one approach is possible. Second hint: It is OK
+% to use numbers as constants in Prolog.)
+%
+% ---/
+
+% We could represent that for example with this fact.
+coordinates(atlanta, 34, 84).
